8ff48ed4e3592be5091c93154ac547c57d6700ac
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-06-30 14:40:31 -0300

    I know a lot of things
    well well well

--- cipher.sh
+++ cipher.sh
@@ -35,63 +35,104 @@ echo "
                             .  %-                           
                                                                                            "
 
-# Script de auditoria de segurança
+# Função para exibir a mensagem de saída
+sair() {
+  echo "
+=============================================
+   Oh não! Bill Cipher! Por favor, deixe-me em paz!
+=============================================
+"
+  exit 0
+}
 
-# Solicita o host a ser verificado
-echo "Digite o host a ser verificado:"
-read host
+# Função para realizar a auditoria de segurança
+auditar_seguranca() {
+  # Solicita o host a ser verificado
+  echo "Digite o host a ser verificado:"
+  read host
 
-# Verificação do whois do host
-echo "Verificação WHOIS do host:"
-whois $host
+  # Verificação do whois do host
+  echo "Verificação WHOIS do host:"
+  whois $host
 
-# Verificação de conexões de rede ativas
-echo "Verificando conexões de rede ativas:"
-netstat -tuln
+  # Obtenção das coordenadas geográficas
+  coordenadas=$(curl -s https://ipinfo.io/$host/loc)
 
-# Verificação de processos em execução
-echo "Verificando processos em execução:"
-ps aux
+  if [ -n "$coordenadas" ]; then
+    echo "Coordenadas geográficas do host: $coordenadas"
+  else
+    echo "Não foi possível obter as coordenadas geográficas do host."
+  fi
 
-# Verificação de portas abertas
-echo "Verificando portas abertas:"
-nmap -p 1-65535 $host
+  # Verificação de conexões de rede ativas
+  echo "Verificando conexões de rede ativas:"
+  netstat -tuln
 
-# Verificação de permissões de arquivos
-echo "Verificando permissões de arquivos:"
-find / -type f -perm /o+w
+  # Verificação de processos em execução
+  echo "Verificando processos em execução:"
+  ps aux
 
-# Verificação de vulnerabilidades conhecidas
-echo "Verificando vulnerabilidades conhecidas:"
-nikto -h $host
+  # Verificação de portas abertas
+  echo "Verificando portas abertas:"
+  nmap -p 1-65535 $host
 
-# Verificação de senhas fracas
-echo "Verificando senhas fracas:"
-john --users=usuarios.txt --wordlist=dicionario.txt
+  # Verificação de permissões de arquivos
+  echo "Verificando permissões de arquivos:"
+  find / -type f -perm /o+w
 
-# Verificação de logs do sistema
-echo "Verificando logs do sistema:"
-cat /var/log/syslog
+  # Verificação de vulnerabilidades conhecidas
+  echo "Verificando vulnerabilidades conhecidas:"
+  nikto -h $host
 
-# Verificação de configurações de segurança
-echo "Verificando configurações de segurança:"
-grep "PermitRootLogin" /etc/ssh/sshd_config
-grep "PasswordAuthentication" /etc/ssh/sshd_config
+  # Verificação de senhas fracas
+  echo "Verificando senhas fracas:"
+  john --users=usuarios.txt --wordlist=dicionario.txt
 
-# Verificação de diretórios e arquivos suspeitos
-echo "Verificando diretórios e arquivos suspeitos:"
-find / -type d \( -name ".git" -o -name ".svn" -o -name ".DS_Store" \) -print
+  # Verificação de logs do sistema
+  echo "Verificando logs do sistema:"
+  cat /var/log/syslog
 
-# Verificação de vulnerabilidades de software
-echo "Verificando vulnerabilidades de software:"
-checksec --all
+  # Verificação de configurações de segurança
+  echo "Verificando configurações de segurança:"
+  grep "PermitRootLogin" /etc/ssh/sshd_config
+  grep "PasswordAuthentication" /etc/ssh/sshd_config
 
-# Verificação de privilégios de usuário
-echo "Verificando privilégios de usuário:"
-id
+  # Verificação de diretórios e arquivos suspeitos
+  echo "Verificando diretórios e arquivos suspeitos:"
+  find / -type d \( -name ".git" -o -name ".svn" -o -name ".DS_Store" \) -print
 
-# Finalização da auditoria de segurança
-echo "Auditoria de segurança concluída."
+  # Verificação de vulnerabilidades de software
+  echo "Verificando vulnerabilidades de software:"
+  checksec --all
 
+  # Verificação de privilégios de usuário
+  echo "Verificando privilégios de usuário:"
+  id
+
+  # Finalização da auditoria de segurança
+  echo "Auditoria de segurança concluída."
+}
+
+# Loop para interagir com o usuário
+while true; do
+  echo "
+Opções:
+1. Realizar auditoria de segurança em um host
+2. Sair
+"
+  read -p "Digite a opção desejada: " opcao
+
+  case $opcao in
+    1)
+      auditar_seguranca
+      ;;
+    2)
+      sair
+      ;;
+    *)
+      echo "Opção inválida. Tente novamente."
+      ;;
+  esac
+done
 
 
