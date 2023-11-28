e51b50488bb428339ea1ac0e5992df39b3a57d10
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-27 23:14:56 -0300

    Atualizar o cipher.py

--- cipher.py
+++ cipher.py
@@ -1,50 +1,123 @@
-    # Verificação de permissões de arquivos
-    executar_comando_e_registrar_saida("find / -type f -perm /o+w", "Verificando permissões de arquivos")
+import subprocess
 
-    # Verificação de vulnerabilidades conhecidas
-    executar_comando_e_registrar_saida(f"nikto -h {host}", "Verificando vulnerabilidades conhecidas")
+def print_ascii_art():
+    ascii_art = """
+    ::::.                           
+                            @@@@=                           
+                            @@@@-                           
+                            @@@@:                           
+                            %@@@.                           
+                         ..:%@@@:::                         
+                         --==+*----                         
+                            .#*-                            
+                           :#  ++                           
+                          =*    =*                          
+                         +=      :#                         
+       .:               *-        .#.              .:       
+     .*+@=-#          .#: :...:..:. #:          *=-%**-     
+      +#%@=          .#.:.   :+    ..*-          :@%%*.     
+        .@.         -# -     =*      :++          %=        
+         @:        =+   :    -*    .:  -*         @:        
+         **    .:=*=     ....:-....     :#=-:    =%         
+          =****+=%-         .  .         .%==+***+.         
+               .#:                        .#:               
+              :#.  .      +=. ..=*      .   *-              
+             -#           #@%+=#@@           ++             
+            =*          . :.     : .          -*            
+           *+      .          .         .      :#           
+          +#============++=========+============+*          
+                       =@:         %#                       
+                      :@-          :@+                      
+                      ##            *@                      
+                      @+   -%:      :@-                     
+                      -#%%#*@= @#=. =@:                     
+                           .@-.@*=*#*:                      
+                            .  %-
+"""
+    print(ascii_art)
 
-    # Verificação de senhas fracas
-    executar_comando_e_registrar_saida("john --users=usuarios.txt --wordlist=dicionario.txt", "Verificando senhas fracas")
+def perform_security_audit():
+    host = input("Digite o host a ser verificado: ")
 
-    # Verificação de logs do sistema
-    executar_comando_e_registrar_saida("cat /var/log/syslog", "Verificando logs do sistema")
+    print("Verificação WHOIS do host:")
+    subprocess.run(["whois", host])
 
-    # Verificação de configurações de segurança
-    executar_comando_e_registrar_saida("grep 'PermitRootLogin' /etc/ssh/sshd_config", "Verificando configurações de segurança (PermitRootLogin)")
-    executar_comando_e_registrar_saida("grep 'PasswordAuthentication' /etc/ssh/sshd_config", "Verificando configurações de segurança (PasswordAuthentication)")
+    print("Obtenção das coordenadas geográficas do host:")
+    try:
+        subprocess.run(["curl", "-s", f"https://ipinfo.io/{host}/loc"], check=True)
+    except subprocess.CalledProcessError:
+        print("Não foi possível obter as coordenadas geográficas do host.")
 
-    # Verificação de diretórios e arquivos suspeitos
-    executar_comando_e_registrar_saida("find / -type d \( -name '.git' -o -name '.svn' -o -name '.DS_Store' \) -print", "Verificando diretórios e arquivos suspeitos")
+    print("Verificando conexões de rede ativas:")
+    subprocess.run(["netstat", "-tuln"])
 
-    # Verificação de vulnerabilidades de software
-    executar_comando_e_registrar_saida("checksec --all", "Verificando vulnerabilidades de software")
+    print("Verificando processos em execução:")
+    subprocess.run(["ps", "aux"])
 
-    # Verificação de privilégios de usuário
-    executar_comando_e_registrar_saida("id", "Verificando privilégios de usuário")
+    print("Verificando portas abertas e versões de serviços:")
+    subprocess.run(["nmap", "-p", "1-65535", "-sV", host])
 
-    # Escaneamento de diretórios
-    executar_comando_e_registrar_saida(f"dirb http://{host} /usr/share/dirb/wordlists/common.txt", "Escaneando diretórios do host")
+    nmap_result = subprocess.run(["nmap", "-p", "1-65535", "--open", "--min-rate=1000", "--max-retries=2", "-oG", "-", host],
+                                 stdout=subprocess.PIPE, text=True, check=True)
 
-    # Finalização da auditoria de segurança
-    with open(log_file, "a") as log:
-        log.write(f"Auditoria de segurança concluída para o host {host}.\n")
+    for line in nmap_result.stdout.splitlines():
+        if "/open/" in line:
+            port = line.split("/")[0]
+            service_version = line.split(" ")[-1]
+            print(f"Procurando exploits para {service_version} na porta {port}:")
+            subprocess.run(["searchsploit", service_version])
 
-# Loop principal do script
-while True:
-    print("""
+    print("Verificando permissões de arquivos:")
+    subprocess.run(["find", "/", "-type", "f", "-perm", "/o+w"])
+
+    print("Verificando vulnerabilidades conhecidas:")
+    subprocess.run(["nikto", "-h", host])
+
+    print("Verificando senhas fracas:")
+    subprocess.run(["john", "--users=usuarios.txt", "--wordlist=dicionario.txt"])
+
+    print("Verificando logs do sistema:")
+    subprocess.run(["cat", "/var/log/syslog"])
+
+    print("Verificando configurações de segurança:")
+    subprocess.run(["grep", "PermitRootLogin", "/etc/ssh/sshd_config"])
+    subprocess.run(["grep", "PasswordAuthentication", "/etc/ssh/sshd_config"])
+
+    print("Verificando diretórios e arquivos suspeitos:")
+    subprocess.run(["find", "/", "-type", "d", "-name", ".git", "-o", "-name", ".svn", "-o", "-name", ".DS_Store", "-print"])
+
+    print("Verificando vulnerabilidades de software:")
+    subprocess.run(["checksec", "--all"])
+
+    print("Verificando privilégios de usuário:")
+    subprocess.run(["id"])
+
+    print("Escaneando diretórios do host:")
+    subprocess.run(["dirb", f"http://{host}", "/usr/share/dirb/wordlists/common.txt"])
+
+    # ... (additional security checks if needed)
+
+    print(f"Auditoria de segurança concluída para o host {host}.")
+
+def main():
+    while True:
+        print("""
   === Script de Auditoria de Segurança ===
 
   Opções:
   1. Realizar auditoria de segurança
   2. Sair
-    """)
+        """)
+        opcao = input("Digite a opção desejada: ")
 
-    opcao = input("Digite a opção desejada: ")
+        if opcao == "1":
+            perform_security_audit()
+        elif opcao == "2":
+            print("Saindo do script. Até logo!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
 
-    if opcao == "1":
-        auditar_seguranca()
-    elif opcao == "2":
-        sair()
-    else:
-        print("Opção inválida. Tente novamente.")
+if __name__ == "__main__":
+    print_ascii_art()
+    main()
